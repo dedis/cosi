951fc5aa032e3f5e29fba8ca29e3d54e8c8ff23a
nikkolasg <nikkolasg@gmail.com>
2016-05-23 16:41:00 +0200

    service test passes :)

--- service/cosi_test.go
+++ service/cosi_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/dedis/cosi/lib"
+	"github.com/dedis/cothority/lib/network"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/dedis/cothority.v0/lib/dbg"
 	"gopkg.in/dedis/cothority.v0/lib/sda"
@@ -16,6 +17,7 @@ func TestServiceCosi(t *testing.T) {
 	// generate 5 hosts, they don't connect, they process messages, and they
 	// don't register the tree or entitylist
 	hosts, el, _ := local.GenTree(5, false, true, false)
+	formatEntityList(local, hosts, el)
 	defer local.CloseAll()
 
 	// Send a request to the service
@@ -26,6 +28,13 @@ func TestServiceCosi(t *testing.T) {
 	dbg.ErrFatal(err, "Couldn't send")
 
 	// verify the response still
-	assert.Nil(t, cosi.VerifySignature(hosts[0].Suite(), msg, el.Aggregate,
-		res.Challenge, res.Response))
+	assert.Nil(t, cosi.VerifySignature(hosts[0].Suite(), el.Publics(),
+		msg, res.Signature))
+}
+
+func formatEntityList(local *sda.LocalTest, h []*sda.Host, el *sda.EntityList) {
+	for i := range el.List {
+		priv := local.GetPrivate(h[i])
+		el.List[i].Public = cosi.Ed25519Public(network.Suite, priv)
+	}
 }
